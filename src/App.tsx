4f1903c30a958cccd53a59b67e66a75ff6c0e069
Wallace <wallace_wolv@hotmail.com>
2021-03-18 18:52:15 -0300

    Carregando transações com context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionProvider } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -21,8 +21,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
-    
+    <TransactionProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionMaodal}/>
 
       <Dashboard />
@@ -33,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionProvider>
   );
 }
