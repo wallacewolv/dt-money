68c69608a8d893b4070b0174f8579865d9784e0d
Wallace <wallace_wolv@hotmail.com>
2021-03-18 14:21:08 -0300

    Alterando as cores do botão ativo com transparentize do polished

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.div`
   h2{
@@ -58,14 +58,23 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
-}
+  activeColor: 'green' | 'red';
+};
+
+const colors = {
+  green: '#33cc95',
+  red: '#E52E4D'
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#aaa' : 'transparent'};
+  background: ${(props) => props.isActive 
+    ?  transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+  };
 
   display: flex;
   align-items: center;
